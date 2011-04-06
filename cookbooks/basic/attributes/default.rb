fe76096c70911bfd7a28914af00159b77c31fe18
Jeffrey ODell <jeffrey.odell@gmail.com>
2011-04-06 16:34:35 +0000

    locale info, attr cleanup

--- cookbooks/basic/attributes/default.rb
+++ cookbooks/basic/attributes/default.rb
@@ -1,1 +1,3 @@
-default[:user] = `echo $USER`.strip
+default[:basic][:user] = 'deploy'
+default[:basic][:locale] = 'en_US.UTF-8'
+default[:basic][:debian_editor] = '/usr/bin/vim.basic'
--- cookbooks/basic/recipes/default.rb
+++ cookbooks/basic/recipes/default.rb
@@ -12,22 +12,29 @@ time
 dnsutils
 lsof
 tree
+language-pack-en-base
 ).each { |p| package p }
 
-execute 'default editor' do
-  command 'update-alternatives --set editor /usr/bin/vim.basic'
-  only_if do platform?('ubuntu', 'debian') end
-  not_if %{update-alternatives --query editor | grep "Value: /usr/bin/vim.basic"}
+execute "Setting locale to #{node.basic.locale}" do
+  command "locale-gen #{node.basic.locale} && update-locale #{node.basic.locale}"
+  only_if { platform?('ubuntu', 'debian') }
+  not_if "locale | grep #{node.basic.locale}"
 end
 
-user node[:user] do
+execute 'default editor' do
+  command "update-alternatives --set editor #{node.basic.debian_editor}"
+  only_if { platform?('ubuntu', 'debian') }
+  not_if %{update-alternatives --query editor | grep "Value: #{node.basic.debian_editor}"}
+end
+
+user node.basic.user do
   shell '/bin/bash'
-  home "/home/#{node[:user]}"
+  home "/home/#{node.basic.user}"
   manage_home true
 end
 
 group 'sudo' do
-  members [ node[:user] ] 
+  members [ node.basic.user ] 
   not_if { `groups | grep sudo` || ENV['USER'] == 'root' }
   only_if { platform?('ubuntu', 'debian') }
 end
